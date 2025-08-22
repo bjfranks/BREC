7d2810c0ddc033a606aecb962275da2b5aa81cd6
Billy Joe Franks <b_franks12@cs.uni-kl.de>
2025-08-22 18:32:04 +0200

    debugging

--- GNNAsKernel/core/model_utils/elements.py
+++ GNNAsKernel/core/model_utils/elements.py
@@ -30,6 +30,7 @@ class DiscreteEncoder(nn.Module):
             x = x.unsqueeze(1)
         out = 0
         for i in range(x.size(1)):
+            print(i, x)
             out = out + self.embeddings[i](x[:, i])
         return out
 
