1b5255a851e5b27380995f56dba3e5b839694532
Billy Joe Franks <b_franks12@cs.uni-kl.de>
2025-08-25 14:53:34 +0200

    debugging

--- GNNAsKernel/test_BREC.py
+++ GNNAsKernel/test_BREC.py
@@ -195,7 +195,8 @@ def evaluation(dataset, device, args): # OUT_PATH unused
             for i, data in enumerate(loader):
                 #torch.set_printoptions(threshold=10_000)
                 #print(data.x)
-                pred = model(data.to(device).type(Int)).detach()
+                data.x = data.x.type(torch.Tensor.int)
+                pred = model(data.to(device)).detach()
                 #print(model.state_dict())
                 pred_0_list.extend(pred[0::2])
                 pred_1_list.extend(pred[1::2])
