95f215e6718a0d0a63271972d4ba0a352c38a8c5
Billy Joe Franks <b_franks12@cs.uni-kl.de>
2025-09-10 10:19:44 +0200

    changed i2gnn to our protocol

--- I2GNN/test_BREC_no4v_60cfi.py
+++ I2GNN/test_BREC_no4v_60cfi.py
@@ -12,7 +12,7 @@ import torch_geometric
 import torch_geometric.loader
 from loguru import logger
 import time
-from BRECDataset_v3_no4v_60cfi import BRECDataset
+from BRECDataset_v3 import BRECDataset
 from tqdm import tqdm
 import os
 from torch_geometric.nn.norm import BatchNorm, LayerNorm
@@ -35,6 +35,10 @@ from dataloader import DataLoader  # use a custom dataloader to handle subgraphs
 from utils import create_subgraphs, create_subgraphs2, check_graphlet
 from count_models import I2GNN
 
+from Xent_Loss import nt_bxent_loss
+import pickle
+from BRECDataset_Wrapper import BRECDataset
+
 
 # torch_geometric.seed_everything(2022)
 NUM_RELABEL = 32
@@ -42,11 +46,11 @@ P_NORM = 2
 OUTPUT_DIM = 16
 EPSILON_MATRIX = 1e-7
 EPSILON_CMP = 1e-6
-SAMPLE_NUM = 340
+SAMPLE_NUM = 600
 EPOCH = 20
 MARGIN = 0.0
 LEARNING_RATE = 1e-4
-THRESHOLD = 72.34
+THRESHOLD = 120.12
 BATCH_SIZE = 16
 WEIGHT_DECAY = 1e-4
 LOSS_THRESHOLD = 0.2
@@ -66,7 +70,9 @@ part_dict = {
     "CFI": (260, 320),
     # "4-Vertex_Condition": (360, 380),
     # "Distance_Regular": (380, 400),
-    "Distance_Regular": (320, 340),
+    "Distance_Regular": (380, 400),
+    "CCoHG": (400, 500),
+    "3r2r": (500, 600),
 }
 parser = argparse.ArgumentParser(description="I2GNN for counting experiments.")
 
@@ -141,6 +147,22 @@ parser.add_argument("--dataset", default="count_cycle")
 parser.add_argument("--load_model", default=None)
 parser.add_argument("--eval", default=0, type=int)
 parser.add_argument("--train_only", default=0, type=int)
+
+parser.add_argument(
+    "--loss",
+    type=str,
+    default="CosineEmbeddingLoss",
+    help="Options are ['CosineEmbeddingLoss', 'nt_bxent_loss']",#TODO
+)
+parser.add_argument("--loss_parameter", type=float, default=1)
+parser.add_argument(
+    '--parts',
+    nargs='+',
+    default=[],#list(part_dict.keys()),
+    help='Options are a subset of '+str(part_dict.keys())
+)
+parser.add_argument("--name_tag", type=str, default=None)
+
 args = parser.parse_args()
 
 P_NORM = 2 if args.P_NORM == "2" else torch.inf
@@ -218,36 +240,74 @@ def get_model(args, device):
 
 # Stage 4: evaluation
 # Here is for evaluation.
-def evaluation(dataset, model, path, device, args):
+def evaluation(dataset, device, args):
+    """
+    When testing on BREC, even on the same graph, the output embedding may be different,
+    because numerical precision problem occur on large graphs, and even the same graph is permuted.
+    However, if you want to test on some simple graphs without permutation outputting the exact same embedding,
+    some modification is needed to avoid computing the inverse matrix of a zero matrix.
+    """
+    # If you want to test on some simple graphs without permutation outputting the exact same embedding, please use S_epsilon.
+    # S_epsilon = torch.diag(
+    #     torch.full(size=(OUTPUT_DIM, 1), fill_value=EPSILON_MATRIX).reshape(-1)
+    # ).to(device)
     def T2_calculation(dataset, log_flag=False):
         with torch.no_grad():
-            loader = DataLoader(dataset, batch_size=BATCH_SIZE)
+            loader = torch_geometric.loader.DataLoader(dataset, batch_size=BATCH_SIZE)
             pred_0_list = []
             pred_1_list = []
             for data in loader:
+                #torch.set_printoptions(threshold=10_000)
+                #print(data.x)
                 pred = model(data.to(device)).detach()
+                #print(model.state_dict())
                 pred_0_list.extend(pred[0::2])
                 pred_1_list.extend(pred[1::2])
+                #print(pred_0_list, pred_1_list)
             X = torch.cat([x.reshape(1, -1) for x in pred_0_list], dim=0).T
             Y = torch.cat([x.reshape(1, -1) for x in pred_1_list], dim=0).T
+            #big = torch.max(torch.max(torch.abs(X)), torch.max(torch.abs(Y)))
+            #X/=big
+            #Y/=big
+            D = X - Y
+            #nanprint(D)
             if log_flag:
                 logger.info(f"X_mean = {torch.mean(X, dim=1)}")
                 logger.info(f"Y_mean = {torch.mean(Y, dim=1)}")
-            D = X - Y
+            D = torch.where(torch.abs(D) < torch.maximum(torch.abs(X), torch.abs(Y))/100, 0, D) # Avoids false positives
+            if log_flag:
+                logger.info(f"newD = {D}")
             D_mean = torch.mean(D, dim=1).reshape(-1, 1)
             S = torch.cov(D)
             inv_S = torch.linalg.pinv(S)
-            return torch.mm(torch.mm(D_mean.T, inv_S), D_mean)
+            # If you want to test on some simple graphs without permutation outputting the exact same embedding, please use inv_S with S_epsilon.
+            # inv_S = torch.linalg.pinv(S + S_epsilon)
+            # print(D, D_mean, S, inv_S)
+            result = NUM_RELABEL*torch.mm(torch.mm(D_mean.T, inv_S), D_mean)
+            if log_flag:
+                logger.info(f"result = {result}")
+            return result
 
     time_start = time.process_time()
 
     # Do something
+    #num_nodes_list = np.load('num_node.npy', allow_pickle=True)
     cnt = 0
     correct_list = []
     fail_in_reliability = 0
     loss_func = CosineEmbeddingLoss(margin=MARGIN)
+    store = []
 
-    for part_name, part_range in part_dict.items():
+    ids = [] #[89, 112, 113, 114, 115, 116, 118, 119, 120, 121, 123, 124, 128, 129, 131, 134, 138,
+           #139, 140, 142, 143, 149, 151, 152, 153, 154, 157, 158, 64, 111, 125, 127, 132, 133,
+           #136, 137, 141, 144, 146, 150, 159, 117, 145, 148, 155, 156, 126, 130, 135, 147, 122]
+
+    file = f"{args.root}/{args.random}_{args.loss}_{str(args.loss_parameter)}.pkl"
+    if args.name_tag is not None:
+        file = f"{args.root}/{args.name_tag}.pkl"
+
+    for part_name in args.parts: #for part_name, part_range in part_dict.items():
+        part_range = part_dict[part_name]
         logger.info(f"{part_name} part starting ---")
 
         cnt_part = 0
@@ -256,51 +316,92 @@ def evaluation(dataset, model, path, device, args):
 
         for id in tqdm(range(part_range[0], part_range[1])):
             logger.info(f"ID: {id}")
-            model = get_model(args, device)
-            optimizer = torch.optim.Adam(
-                model.parameters(), lr=LEARNING_RATE, weight_decay=WEIGHT_DECAY
-            )
-            scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer)
-            dataset_traintest = dataset[
-                id * NUM_RELABEL * 2 : (id + 1) * NUM_RELABEL * 2
-            ]
-            dataset_reliability = dataset[
-                (id + SAMPLE_NUM)
-                * NUM_RELABEL
-                * 2 : (id + SAMPLE_NUM + 1)
-                * NUM_RELABEL
-                * 2
-            ]
-            model.train()
-            for _ in range(EPOCH):
-                traintest_loader = DataLoader(
-                    dataset_traintest, batch_size=BATCH_SIZE
+            time_start = time.process_time()
+            epochs = 0
+            for test_count in range(10):
+                dataset_traintest = dataset[
+                    id * NUM_RELABEL * 2 : (id + 1) * NUM_RELABEL * 2
+                ]
+                model = get_model(args, dataset_traintest[0].num_nodes, 1, device)  # num_nodes_list[id]
+                optimizer = torch.optim.Adam(
+                    model.parameters(), lr=LEARNING_RATE, weight_decay=WEIGHT_DECAY
                 )
-                loss_all = 0
-                for data in traintest_loader:
-                    optimizer.zero_grad()
-                    pred = model(data.to(device))
-                    # print(pred.size())
-                    # print(len(pred))
-                    loss = loss_func(
-                        pred[0::2],
-                        pred[1::2],
-                        torch.tensor([-1] * (len(pred) // 2)).to(device),
+                scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+                    optimizer)  # StepLR(optimizer, gamma=0.5, step_size=250)
+                dataset_reliability = dataset[
+                    (id + SAMPLE_NUM)
+                    * NUM_RELABEL
+                    * 2 : (id + SAMPLE_NUM + 1)
+                    * NUM_RELABEL
+                    * 2
+                ]
+                model.train()
+                for _ in range(EPOCH):
+                    traintest_loader = torch_geometric.loader.DataLoader(
+                        dataset_traintest, batch_size=BATCH_SIZE
                     )
-                    # print(loss)
-                    loss.backward()
-                    optimizer.step()
-                    loss_all += len(pred) / 2 * loss.item()
-                loss_all /= NUM_RELABEL
-                logger.info(f"Loss: {loss_all}")
-                if loss_all < LOSS_THRESHOLD:
-                    logger.info("Early Stop Here")
-                    break
-                scheduler.step(loss_all)
+                    loss_all = 0
+                    for data in traintest_loader:
+                        if id in ids:
+                            import networkx as nx
+                            import matplotlib.pyplot as plt
+                            print(id)
+                            ids.remove(id)
+                            for datum in data.to_data_list():
+                                g = torch_geometric.utils.to_networkx(datum, to_undirected=True)
+                                nx.draw(g)
+                                plt.show()
 
-            model.eval()
-            T_square_traintest = T2_calculation(dataset_traintest, True)
-            T_square_reliability = T2_calculation(dataset_reliability, True)
+                        optimizer.zero_grad()
+                        #print(data.x)
+                        pred = model(data.to(device))
+                        #print(pred)
+                        apart = loss_func(
+                            pred[0::2],
+                            pred[1::2],
+                            torch.tensor([-1] * (len(pred) // 2)).to(device),
+                        )
+                        together = loss_func(
+                            torch.cat((pred[0::4], pred[1::4])),
+                            torch.cat((pred[2::4], pred[3::4])),
+                            torch.tensor([1] * (len(pred) // 2)).to(device),
+                        )
+                        # print(apart, together)
+                        a = args.loss_parameter
+                        if args.loss == "CosineEmbeddingLoss":
+                            loss = a*apart+(1-a)*together
+                        elif args.loss == "nt_bxent_loss":
+                            loss = nt_bxent_loss(pred,
+                                torch.tensor([(2*a, 2*b) for a in range(len(pred)//2) for b in range(len(pred)//2)] +
+                                [(2*a+1, 2*b+1) for a in range(len(pred)//2) for b in range(len(pred)//2)]).to(device),
+                                                 0.5, device)
+                        loss.backward()
+                        optimizer.step()
+                        loss_all += len(pred) / 2 * loss.item()
+                    #if _%5 == 0:
+                    #    print(loss_all)  # TODO remove
+                    #for name, param in model.named_parameters():
+                    #    if param.requires_grad:
+                    #        print(name, param.data)
+                    #        break
+                    loss_all /= NUM_RELABEL
+                    logger.info(f"Loss: {loss_all}")
+                    if loss_all < LOSS_THRESHOLD:
+                        logger.info("Early Stop Here")
+                        break
+                    scheduler.step(loss_all)
+                epochs += _
+
+                model.eval()
+                T_square_traintest = T2_calculation(dataset_traintest, True)
+                T_square_reliability = T2_calculation(dataset_reliability, True)
+                print(T_square_traintest, T_square_reliability)
+
+                if (T_square_traintest > THRESHOLD and T_square_reliability < THRESHOLD
+                        and not torch.isclose(T_square_traintest, T_square_reliability, atol=EPSILON_CMP)):
+                    break
+            time_end = time.process_time()
+            time_cost = round(time_end - time_start, 2)
 
             isomorphic_flag = False
             reliability_flag = False
@@ -321,6 +422,11 @@ def evaluation(dataset, model, path, device, args):
                 fail_in_reliability_part += 1
             logger.info(f"isomorphic: {isomorphic_flag} {T_square_traintest}")
             logger.info(f"reliability: {reliability_flag} {T_square_reliability}")
+            #print(isomorphic_flag, reliability_flag, T_square_traintest, T_square_reliability)
+
+            #save to file here
+            store.append((part_name, id, isomorphic_flag, T_square_traintest, reliability_flag, T_square_reliability,
+                          test_count+1, time_cost, epochs))
 
         end = time.process_time()
         time_cost_part = round(end - start, 2)
@@ -332,6 +438,11 @@ def evaluation(dataset, model, path, device, args):
             f"Fail in reliability: {fail_in_reliability_part} / {part_range[1] - part_range[0]}"
         )
 
+        store.append(args)
+        with open(file, 'ab') as f:
+            pickle.dump(store, f)
+        store = []
+
     time_end = time.process_time()
     time_cost = round(time_end - time_start, 2)
     logger.info(f"evaluation time cost: {time_cost}")
@@ -342,12 +453,12 @@ def evaluation(dataset, model, path, device, args):
     logger.info(f"Fail in reliability: {fail_in_reliability} / {SAMPLE_NUM}")
     logger.info(correct_list)
 
-    logger.add(f"{path}/result_show.txt", format="{message}", encoding="utf-8")
+    logger.add(f"{args.root}/{args.name_tag}_show.log", format="{message}", encoding="utf-8")
     logger.info(
-        "Real_correct\tCorrect\tFail\th\tlayers\tnode_label\tuse_rd\tOUTPUT_DIM\tBATCH_SIZE\tLEARNING_RATE\tWEIGHT_DECAY\tSEED"
+        "Real_correct\tCorrect\tFail\tnum_layers\thidden_units\tnum_runs\tOUTPUT_DIM\tBATCH_SIZE\tLEARNING_RATE\tWEIGHT_DECAY\tSEED"
     )
     logger.info(
-        f"{cnt-fail_in_reliability}\t{cnt}\t{fail_in_reliability}\t{args.h}\t{args.layers}\t{args.node_label}\t{args.use_rd}\t{OUTPUT_DIM}\t{BATCH_SIZE}\t{LEARNING_RATE}\t{WEIGHT_DECAY}\t{SEED}"
+        f"{cnt-fail_in_reliability}\t{cnt}\t{fail_in_reliability}\t{args.num_layers}\t{args.hidden_units}\t{args.num_runs}\t{OUTPUT_DIM}\t{BATCH_SIZE}\t{LEARNING_RATE}\t{WEIGHT_DECAY}\t{SEED}"
     )
 
 
@@ -368,8 +479,8 @@ def main():
 
     pre_calculation()
     dataset = get_dataset(name=DATASET_NAME, device=device)
-    model = get_model(args, device)
-    evaluation(dataset, model, OUT_PATH, device, args)
+    #model = get_model(args, device)
+    evaluation(dataset, device, args)
 
 
 if __name__ == "__main__":
